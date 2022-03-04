494077915e243c80a0868f1f91c15080c11652eb
Frenky Harry S. Sinaga <me@frenky.id>
2022-03-04 15:42:59 +0700

    first commit: done tutorial

---
+++ pages/posts/[id].tsx
@@ -0,0 +1,50 @@
+import Layout from "../../components/layout";
+import Head from "next/head";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import { GetStaticPaths, GetStaticProps } from "next";
+
+export default function Post({
+  postData,
+}: {
+  postData: {
+    title: string;
+    date: string;
+    contentHtml: string;
+  };
+}) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+};
